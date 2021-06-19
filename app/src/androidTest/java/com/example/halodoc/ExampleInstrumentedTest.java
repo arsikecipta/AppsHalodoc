5cd790cef13109f1e8c5557816ea47b71fdef56f
arsikecipta <arsikepelangi15@gmail.com>
2021-06-19 12:17:30 +0700

    bismillah fix

--- app/src/androidTest/java/com/example/halodoc/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/halodoc/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.appshalodoc;
+package com.example.halodoc;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.appshalodoc", appContext.getPackageName());
+        assertEquals("com.example.halodoc", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/example/halodoc/MainActivity.java
+++ app/src/main/java/com/example/halodoc/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.appshalodoc;
+package com.example.halodoc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
